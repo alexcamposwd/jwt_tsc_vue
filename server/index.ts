ab5cde5ed3e45ae45b232431d1af90f888d9a60d
Alex Campos <alexfilhotes@hotmail.com>
2022-03-16 16:23:05 -0300

    Update port to PORT

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import app from './app'
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log(`Server running http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running http://localhost:${PORT}`);
 })
